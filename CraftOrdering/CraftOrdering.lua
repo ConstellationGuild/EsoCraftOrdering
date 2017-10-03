a9385d286a5157976895af4ba86d726383b02c42
Hugh Wood <hugh.wood@rencore.com>
2017-10-03 17:29:46 +0100

    Removed bad encoding from helper methods, refactored all helper methods to file, unit test registration and calling added

--- CraftOrdering/CraftOrdering.lua
+++ CraftOrdering/CraftOrdering.lua
@@ -30,9 +30,9 @@ CraftOrdering.RealVersion = 1.0
 ECO.loaded = false
 ECO.debug = true
 
-local orange = "|cFFA400"
-local green = "|c00A400"
-local red = "|cFF0000"
+ECO.orange = "|cFFA400"
+ECO.green = "|c00A400"
+ECO.red = "|cFF0000"
 
 ------------------------------------------------------------------
 --  OnAddOnLoaded  --
@@ -47,20 +47,16 @@ local function OnAddOnLoaded(_event, addonName)
 end
 
 ------------------------------------------------------------------
---  Debug mode output to chat  --
-------------------------------------------------------------------
-function ECO.d(t)
-  if ECO.debug then
-    d(t)
-  end
-end
-
-------------------------------------------------------------------
 --  OnPlayerActivated  --
 ------------------------------------------------------------------
 local function OnPlayerActivated(eventCode)
   if ECO.loaded then return end
-  ECO.d(green .. "CraftOrdering Loaded")
+  ECO.d(ECO.green .. "CraftOrdering Loaded")
+
+  if ECO.debug then
+    CraftOrdering:ExecuteUnitTests()
+  end
+  
   ECO.loaded = true
 end
 
@@ -69,9 +65,6 @@ end
 ------------------------------------------------------------------
 function CraftOrdering:Initialize()
   EVENT_MANAGER:RegisterForEvent(CraftOrdering.name, EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
-  if ECO.debug then
-    CraftOrdering:ExecuteUnitTests()
-  end
 end
 
 ----------------------------------------------------------------------
--- CraftOrdering/UnitTesting.lua
+++ CraftOrdering/UnitTesting.lua
@@ -1,17 +1,30 @@
-CraftOrdering.Tests = {
-  Serialisation = UnitTesting.Tests:Serialisation
-}
 
-function CraftOrdering:ExecuteUnitTests()
-  for i in CraftOrdering.Tests do
-    i()
-  end
-end
-
-function CraftOrdering.Tests:Serialisation()
-  local testObject = {one="one",two="two"}
-  local outout = CraftOrdering:Serialisation(testObject)
+------------------------------------------------------------------
+--  Tests  --
+------------------------------------------------------------------
+function Serialisation()
+  local testObject = {["one"]="one", ["two"]="two"}
+  local outout = CraftOrdering:Serialize(testObject)
   ECO.d(output)
   --local condition = output == "{\"one\",\"two\"}"
   return condition == true
+end
+
+------------------------------------------------------------------
+--  Register Tests  --
+------------------------------------------------------------------
+CraftOrdering.Tests = {
+  ['Serialisation'] = Serialisation
+}
+
+------------------------------------------------------------------
+--  Control / Helper methods  --
+------------------------------------------------------------------
+function CraftOrdering:ExecuteUnitTests()
+  d(ECO.green.."Initiating unit tests")
+  for i,v in pairs(CraftOrdering.Tests) do
+    d(ECO.orange..i)
+    v()
+  end
+  d(ECO.green.."Unit Testing Complete")
 end
