e4c6ab113407868113bba54561b7d2db365d5a1a
Klaus Andersen <klausreinholtandersen@gmail.com>
2017-10-09 20:46:30 +0200

    Added close button, mouse captured on elements

--- CraftOrdering/CraftOrderingPanel.lua
+++ CraftOrdering/CraftOrderingPanel.lua
@@ -3,8 +3,10 @@ end
 
 function CraftOrderingPanelInitialize()
 end
+
 local hidden = true;
 function CraftOrderingPanelShow()
   hidden = not hidden;
   CraftOrderingPanel:SetHidden(hidden)
+  SetGameCameraUIMode(not hidden)
 end
